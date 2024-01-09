2f3fe492c7a488aa7bd0c5a224a0745ed37382eb
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 22:19:04 +0000

    6/12

--- myApp.js
+++ myApp.js
@@ -1,53 +1,24 @@
 let express = require('express');
 let app = express();
-require('dotenv').config()
-console.log("Hello World")
+require('dotenv').config();
 
-
+console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
-/* app.get("/", function(req, res) {
-     res.send("Hello Express");
-   }); */
-   absolutePath = __dirname + '/views/index.html'
-   app.get("/", function(req, res) {
-       res.sendFile(absolutePath);
-  });
 
-  
+let absolutePath = __dirname + '/views/index.html';
+app.get("/", function(req, res) {
+    res.sendFile(absolutePath);
+});
 
-
-  app.get('/json', function (req, res) {
+app.get('/json', function (req, res) {
     let responseMessage = "Hello json";
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        responseMessage  = responseMessage.toUpperCase();
-      }
+        responseMessage = responseMessage.toUpperCase();
+    }
 
     res.json({"message": responseMessage});
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
