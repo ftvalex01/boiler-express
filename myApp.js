07fbe1859ecf29fe3ba066d32fdcd1f817c30778
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 22:52:32 +0000

    6/12 definitivo x-1

--- myApp.js
+++ myApp.js
@@ -20,11 +20,15 @@ app.get('/', (rq, rs) => {
   
 });
 
-
-app.get("/json", (req, res) => {
-    var message = 'Hello json';
-    if ( process.env.MESSAGE_STYLE === "uppercase") {
-          message = message.toUpperCase();
-    }
-    res.json({ "message": message });
-  });
+app.use("/json", (req, res) => {
+       let message = "Hello json";
+      
+       if(process.env.MESSAGE_STYLE==="uppercase"){
+         message = message.toUpperCase();
+      }
+      else{
+        message = message;
+      }
+      return res.json({"message":message})
+     });
+    
