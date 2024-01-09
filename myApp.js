d8dba812a6ab711994fd4a3bfaf1669921536ac5
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 22:41:17 +0000

    6/12 definitivo

--- myApp.js
+++ myApp.js
@@ -1,32 +1,38 @@
-require('dotenv').config();
-let express = require('express');
-let app = express();
-
-console.log("Hello World");
-
-app.use("/public", express.static(__dirname + "/public"));
-
-let absolutePath = __dirname + '/views/index.html';
-app.get("/", function(req, res) {
-    res.sendFile(absolutePath);
-});
+var express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
 
 const note = {
-    "message": "Hello json"
+  "message":"Hello json"
+};
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use((rq, rs, next) => {
+
+  console.log(`${rq.method} ${rq.path} - ${rq.ip}`);
+  next();
+  
+});
+
+app.get('/', (rq, rs) => {
+
+  console.log( "Hello World" );
+  rs.sendFile( __dirname + '/views/index.html' );
+  
+});
+
+
+app.get('/json', (rq, rs) => {
+
+  if(process.env.MESSAGE_STYLE == 'uppercase'){
+
+    note.message = note.message.toUpperCase();
+    
   };
 
-app.get('/json', (req, res) => {
-
-    if(process.env.MESSAGE_STYLE == 'uppercase'){
-        
-      note.message = note.message.toUpperCase();
-      
-    }else{
-        note.message
-    }
+  rs.json(note);
   
-    res.json(note);
-    
-  });
-
-module.exports = app;
+});
