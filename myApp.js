7ce095f68b4b468543f35267d618b05d50733c17
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 22:14:20 +0000

    6/12

--- myApp.js
+++ myApp.js
@@ -21,9 +21,7 @@ app.use("/public", express.static(__dirname + "/public"));
     let responseMessage = "Hello json";
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        response = responseMessage.toUpperCase();
-      } else {
-        response = "Hello World";
+        responseMessage  = responseMessage.toUpperCase();
       }
 
     res.json({"message": responseMessage});
