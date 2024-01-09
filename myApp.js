b9500050748541fb92c03599a883b48fb730a117
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 21:59:11 +0000

    6/12

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-
+require('dotenv').config()
 console.log("Hello World")
 
 
@@ -17,9 +17,14 @@ app.use("/public", express.static(__dirname + "/public"));
   
 
 
-
   app.get('/json', function (req, res) {
-    res.json( {"message": "Hello json"} )
+    let responseMessage = "Hello json";
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        responseMessage = responseMessage.toUpperCase();
+    }
+
+    res.json({"message": responseMessage});
 });
 
 
