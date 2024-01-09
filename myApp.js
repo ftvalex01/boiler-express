68190704104ce602114112de2e67891cb1fe07c7
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 21:50:07 +0000

    4/12

--- myApp.js
+++ myApp.js
@@ -4,8 +4,8 @@ let app = express();
 console.log("Hello World")
 
 
-app.use(express.static(__dirname + "/public"));
 
+app.use("/public", express.static(__dirname + "/public"));
 /* app.get("/", function(req, res) {
      res.send("Hello Express");
    }); */
