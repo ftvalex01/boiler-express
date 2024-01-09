1862ed5b47039e04f981b551ed04e37446c29844
ftvalex01 <ftvalex@hotmail.com>
2024-01-09 23:52:23 +0000

    12/12

--- myApp.js
+++ myApp.js
@@ -61,8 +61,8 @@ app.get("/:word/echo", function (req, res) {
 });
 app.get("/name", function (req, res) {
 
-    const firstName = req.query.first;
-    const lastName = req.query.last;
+    const firstName = req.body.first;
+    const lastName = req.body.last;
 
 
     res.json({ name: `${firstName} ${lastName}` });
